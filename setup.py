b911211f5d2df9a3caf353de42122b4bb46d6860
alcantarar <ryan.alcantara@colorado.edu>
2019-10-03 12:18:40 -0700

    add license, basic setup and sample files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Detrendr',
+    version='1.0',
+    packages=['detrendr',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+    description='Detrendr package for running biomechanics data',
+    author='Ryan Alcantara',
+    author_email='ryan.alcantara@colorado.edu',
+    url='https://github.com/alcantarar/detrend_force',
+)
