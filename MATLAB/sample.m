a82ecb4bc5f2158ed157c4625df0ca936710193e
alcantarar <ryan.alcantara@colorado.edu>
2019-10-06 23:00:23 -0600

    finished mean_aerial_force matlab

--- MATLAB/sample.m
+++ MATLAB/sample.m
@@ -26,3 +26,5 @@ aerial_end = step_begin(2:end);
 % Must trim beginning and end of aerial phase to get true aerial phase
 % value. Filtering smooths out rapid transitions at start/end.
 trim = trim_aerial(GRF_filt(:,3), step_begin, step_end);                         
+aerial_means = mean_aerial_force(GRF_filt(:,3), step_begin, step_end, trim);
+
