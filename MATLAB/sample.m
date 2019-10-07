574b4940fd33eae096676fc8bc20444a4aa2884d
alcantarar <ryan.alcantara@colorado.edu>
2019-10-07 00:18:21 -0600

    matlab port complete. detrend fxn works. improve last step

--- MATLAB/sample.m
+++ MATLAB/sample.m
@@ -27,3 +27,15 @@ aerial_end = step_begin(2:end);
 % value. Filtering smooths out rapid transitions at start/end.
 trim = trim_aerial(GRF_filt(:,3), step_begin, step_end);                         
 aerial_means = mean_aerial_force(GRF_filt(:,3), step_begin, step_end, trim);
+plot_aerial(GRF_filt(:,3), aerial_means, aerial_begin, aerial_end, trim)
+
+% Detrend signal
+[vGRF_detrend, aerial_means_detrend] = detrend(GRF_filt(:,3),... %nx1 force array
+    Fs,... %force sampling frequency
+    aerial_means,... %mean force during aerial phase
+    step_begin,... %tc_begin
+    step_end,... %tc_end
+    trim,... %trim off beginning and end of aerial phase
+    1); %(d)isplay plots = True
+
+% I think this process can be repeated over and over. Should try. 
--- dryft/signal.py
+++ dryft/signal.py
@@ -108,7 +108,7 @@ def detrend(force_f, Fs, aerial_means, step_begin, step_end, trim, plot=False):
     for i in range(aerial_begin.shape[0]):
         aerial_means_d[i] = np.mean(
             force_fd[aerial_begin[i] + trim:aerial_end[i] - trim,])
-    aerial_means_d = aerial_means_d[aerial_means_d != 0.0]
+    aerial_means_d = aerial_means_d[aerial_means_d != 0.0] # why is this here?
 
     if plot:
         # plot detrend vs old mean aerial
