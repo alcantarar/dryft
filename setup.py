5d83737c4b349bcee7383a975e4b93aeafa201c9
alcantarar <ryan.alcantara@colorado.edu>
2019-10-30 00:19:51 -0600

    update signal.py to work with pypi. uploaded 1.0.1 to test

--- setup.py
+++ setup.py
@@ -1,37 +1,14 @@
-# from distutils.core import setup
-#
-# setup(
-#     name='dryft',
-#     version='1.0',
-#     packages=['dryft'],
-#     license='MIT License',
-#     long_description=open('README.md').read(),
-#     description='Dryft package for running biomechanics data',
-#     author='Ryan Alcantara',
-#     author_email='ryan.alcantara@colorado.edu',
-#     url='https://github.com/alcantarar/dryft',
-# )
+from setuptools import setup, find_packages
+setup(
+    name = "dryft",
+    version = "1.0.1",
+    packages = find_packages(),
+    # metadata for upload to PyPI
+    author = "Ryan Alcantara",
+    author_email = "ryan.alcantara@colorado.edu",
+    description = "This is an Example Package",
+    license = "MIT",
+    url = "http://github.com/alcantarar/dryft",   # project home page, if any
 
-
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="dryft", # Replace with your own username
-    version="1.0",
-    author="Ryan Alcantara",
-    author_email="ryan.alcantara@colorado.edu",
-    description="A package for running biomechanics force data",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/alcantarar/dryft",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    # could also include long_description, download_url, classifiers, etc.
 )
