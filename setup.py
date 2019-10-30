2bc829694a4deceebacc726f29e26dd90ed333a9
alcantarar <ryan.alcantara@colorado.edu>
2019-10-30 00:33:37 -0600

    pypi 1.0.2 has setup.py that calls README.md

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name = "dryft",
-    version = "1.0.1",
+    version = "1.0.2",
     packages = find_packages(),
     # metadata for upload to PyPI
     author = "Ryan Alcantara",
     description = "A package to process biomechanical running data",
-    long_description = open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author_email = "ryan.alcantara@colorado.edu",
     license = "MIT",
     url = "http://github.com/alcantarar/dryft",   # project home page, if any
