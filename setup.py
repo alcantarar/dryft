7349144d7c713be47411a1576a94431651c8e09d
alcantarar <ryan.alcantara@colorado.edu>
2019-10-29 22:54:18 -0600

    update setup.py with new url

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='Detrendr',
     version='1.0',
-    packages=['detrendr',],
+    packages=['dryft',],
     license='MIT License',
     long_description=open('README.md').read(),
     description='Detrendr package for running biomechanics data',
     author='Ryan Alcantara',
     author_email='ryan.alcantara@colorado.edu',
-    url='https://github.com/alcantarar/detrend_force',
+    url='https://github.com/alcantarar/dryft',
 )
