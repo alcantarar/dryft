6a67f5930a4023c890fd02403cecd99adda1d3fc
alcantarar <ryan.alcantara@colorado.edu>
2019-10-30 10:51:06 -0600

    move plotting funxtinos to own submodule

--- sample/sample.py
+++ sample/sample.py
@@ -1,4 +1,4 @@
-import dryft.signal as dr
+from dryft import signal, plot
 import pandas as pd
 from scipy.signal import butter, filtfilt
 import matplotlib.pyplot as plt
@@ -13,13 +13,13 @@ b,a = butter(2, Fc/Fn)
 GRF_filt = filtfilt(b, a, GRF, axis=0)  # filtfilt doubles order (2nd*2 = 4th order effect)
 
 # Identify where stance phase occurs (foot on ground)
-step_begin, step_end = dr.splitsteps(vGRF=GRF_filt[:,2],
+step_begin, step_end = signal.splitsteps(vGRF=GRF_filt[:,2],
                                   threshold=110,
                                   Fs=300,
                                   min_tc=0.2,
                                   max_tc=0.4,
                                   plot=False)
-# dr.plotsteps(GRF_filt[:,2], step_begin, step_end)
+# plot.stance(GRF_filt[:,2], step_begin, step_end)
 
 # Identify where aerial phase occurs (feet not on ground)
 aerial_begin_all = step_end[:-1]
@@ -28,12 +28,12 @@ print('Number of aerial begin/end:', aerial_begin_all.shape[0], aerial_end_all.s
 
 # Determine average force signal during aerial phase
 # Must trim beginning and end of aerial phase to get true aerial phase value
-trim = dr.trimaerial(GRF_filt[:,2], step_begin, step_end)
-aerial_means = dr.meanaerialforce(GRF_filt[:,2], step_begin, step_end, trim ) #aerial_means will be same width as GRF_filt
-dr.plotaerial(GRF_filt[:,2], aerial_means, aerial_begin_all, aerial_end_all, trim) #aerial_means and GRF_filt must be (n,) arrays
+trim = signal.trimaerial(GRF_filt[:,2], step_begin, step_end)
+aerial_means = signal.meanaerialforce(GRF_filt[:,2], step_begin, step_end, trim ) #aerial_means will be same width as GRF_filt
+plot.aerial(GRF_filt[:,2], aerial_means, aerial_begin_all, aerial_end_all, trim) #aerial_means and GRF_filt must be (n,) arrays
 
 # Detrend signal
-force_fd, aerial_means_d = dr.detrend(GRF_filt[:,2],
+force_fd, aerial_means_d = signal.detrend(GRF_filt[:,2],
                                           Fs,
                                           aerial_means,
                                           step_begin,
