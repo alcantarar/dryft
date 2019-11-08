bdb069a045cb1833cb7159cc21cdf655f378a251
alcantarar <ryan.alcantara@colorado.edu>
2019-11-08 13:37:28 -0700

    cleanup sample comments

--- sample/sample.py
+++ sample/sample.py
@@ -3,6 +3,7 @@ import pandas as pd
 from scipy.signal import butter, filtfilt
 import matplotlib.pyplot as plt
 import numpy as np
+
 # Read in data from force plate
 GRF = pd.read_csv('drifting_forces.txt', header=None)
 
@@ -21,17 +22,18 @@ step_begin, step_end = signal.splitsteps(vGRF=GRF_filt[:,2],
                                   max_tc=0.4,
                                   plot=False)
 # plot.stance(GRF_filt[:,2], step_begin, step_end)
+# *Step_begin and step_end can be used to detrend other columns of GRF_filt as well*
 
 # Determine force signal at middle of aerial phase (feet not on ground)
-aerial_vals, aerial_loc = signal.aerialforce(GRF_filt[:,2], step_begin, step_end) #aerial_vals will be same width as GRF_filt
+aerial_vals, aerial_loc = signal.aerialforce(GRF_filt[:,2], step_begin, step_end)
+
 # Plot all aerial phases to see what is being subtracted from signal in signal.detrend()
-# plot.aerial(GRF_filt[:,2], aerial_vals, aerial_loc, step_begin, step_end) #aerial_vals and GRF_filt must be (n,) arrays
+plot.aerial(GRF_filt[:,2], aerial_vals, aerial_loc, step_begin, step_end)
 
 # Detrend signal
 force_fd = signal.detrend(GRF_filt[:,2], aerial_vals, aerial_loc)
 
-
-# compare detrended signal to original
+# Compare detrended signal to original
 step_begin_d, step_end_d = signal.splitsteps(vGRF=force_fd,
                                              threshold=10,
                                              Fs=300,
@@ -40,8 +42,7 @@ step_begin_d, step_end_d = signal.splitsteps(vGRF=force_fd,
                                              plot=False)
 aerial_vals_d, aerial_loc_d = signal.aerialforce(force_fd, step_begin_d, step_end_d)
 
-
-# plot original vs detrended signal
+# Plot waveforms (original vs detrended)
 plt.detrendp, (plt1, plt2) = plt.subplots(2, 1, figsize=(15, 7))
 plt1.plot(np.linspace(0, force_fd.shape[0] / Fs, force_fd.shape[0]),
              GRF_filt[:,2],
@@ -56,7 +57,7 @@ plt1.legend(['original signal', 'detrended signal'], loc=1)
 plt1.set_xlabel('Seconds')
 plt1.set_ylabel('force (N)')
 
-# plot detrend vs original aerial phases
+# Plot aerial phases (original vs detrended)
 plt2.set_title('Aerial Phases')
 plt2.set_xlabel('step')
 plt2.set_ylabel('force (N)')
